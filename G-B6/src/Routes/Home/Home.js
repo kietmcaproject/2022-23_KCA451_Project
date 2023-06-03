513ea2d4f9a373ab3ecace1da28049d448957b96
Vidhish21 <91197520+Vidhish21@users.noreply.github.com>
2023-06-03 11:15:02 +0530

    Delete G-B6/src directory

--- G-B6/src/Routes/Home/Home.js
+++
@@ -1,8 +0,0 @@
-// importing homepage component
-import { HomePage } from "../../components/Homepage/Homepage";
-
-export default function Home(){
-    return(
-        <HomePage />
-    );
-}
--- G-B6/src/components/PizzaCard/ToppingCard/ToppingCard.js
+++
@@ -1,7 +0,0 @@
-export function ToppingCard(props){
-    return(
-        <div className="mx-2 bg-gray-300 p-1 shadow-md text-sm rounded-md">
-        {props.topping.name}
-        </div>
-    );
-}
